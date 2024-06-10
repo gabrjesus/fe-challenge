6752c7c635e08ee112e65a062fae383ddb5539a6
--global <gabrieldejesus.dev@gmail.com>
2024-06-10 13:13:24 -0300

    Setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, HostBinding } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { MatMenuModule } from '@angular/material/menu';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    MatToolbarModule,
+    MatIconModule,
+    MatSlideToggleModule,
+    ReactiveFormsModule,
+    MatMenuModule,
+    MatButtonModule
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss',
+  styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'fe-challenge';
+  @HostBinding('class')
+  currentTheme: 'light-theme' | 'dark-theme' = 'dark-theme';
+
+  isDarkMode: boolean = true;
+
+  title = 'Rick and Morty!';
+
+  onThemeChanged() {
+    this.isDarkMode = !this.isDarkMode;
+
+    if (this.isDarkMode) {
+      this.currentTheme = 'dark-theme';
+    } else {
+      this.currentTheme = 'light-theme';
+    }
+  }
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -6,5 +6,5 @@ import { provideAnimationsAsync } from '@angular/platform-browser/animations/asy
 import { provideStore } from '@ngrx/store';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideAnimationsAsync(), provideStore()],
+  providers: [provideRouter(routes), provideAnimationsAsync(), provideStore()]
 };
